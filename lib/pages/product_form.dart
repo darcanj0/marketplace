6fff13f6a9896df1d29dc6193e1aa07a79e7ff50
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-10-06 02:25:50 -0300

    feat: add image loader to input field

--- lib/pages/product_form.dart
+++ lib/pages/product_form.dart
@@ -10,13 +10,26 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final FocusNode priceFocus = FocusNode();
   final FocusNode descriptionFocus = FocusNode();
+  final FocusNode imageUrlFocus = FocusNode();
+  final TextEditingController imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    imageUrlFocus.addListener(loadImage);
+    super.initState();
+  }
 
   @override
   void dispose() {
     priceFocus.dispose();
     descriptionFocus.dispose();
+    imageUrlController.removeListener(loadImage);
+    imageUrlController.dispose();
+    imageUrlFocus.dispose();
     super.dispose();
   }
+
+  void loadImage() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +64,37 @@ class _ProductFormPageState extends State<ProductFormPage> {
               decoration: const InputDecoration(
                 labelText: 'Description',
               ),
+              onFieldSubmitted: (value) {
+                imageUrlFocus.requestFocus();
+              },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    onTapOutside: (_) => loadImage(),
+                    controller: imageUrlController,
+                    focusNode: imageUrlFocus,
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    decoration: const InputDecoration(
+                      labelText: 'Image URL',
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: const EdgeInsets.only(top: 10, left: 10),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.grey)),
+                  alignment: Alignment.center,
+                  child: imageUrlController.text.isEmpty
+                      ? const Text('Enter Url')
+                      : Image.network(imageUrlController.text),
+                )
+              ],
             ),
           ],
         )),
