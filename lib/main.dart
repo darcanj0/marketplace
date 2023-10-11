e3adcc78b23de790b6c88d9f023dbbbe332f4084
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-10-11 11:24:54 -0300

    config: add app to firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,12 @@ import 'package:clothing/constants/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider with ChangeNotifier {
+  Future<void> login() async {}
+  Future<void> signup() async {}
+}
