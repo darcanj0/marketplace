29c12703628f44f9419c7a81b6e5deb5e80efed7
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-10-13 18:58:29 -0300

    refactor: remove unused code

--- lib/constants/server.dart
+++ lib/constants/server.dart
@@ -1,11 +1,1 @@
-class ServerConstants {
-  static const String domain = 'marketplace-e37b2-default-rtdb.firebaseio.com';
-  static String getAndCreatePath(DbPaths path) => '/${path.name}.json';
-  static String updateAndDeletePath(DbPaths path, String id) =>
-      '/${path.name}/$id.json';
-
-  static String dbPath(DbPaths collection, String id) =>
-      '${collection.name}/$id';
-}
-
 enum DbPaths { products, orders, userFavorites }
