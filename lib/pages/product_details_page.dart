9d1bf3e7196be33e55961ce107736197d9015b08
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-10-15 12:01:13 -0300

    fix: improve readability

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -14,25 +14,43 @@ class ProductDetailsPage extends StatelessWidget {
     final TextTheme textTheme = theme.textTheme;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                tag: product.id,
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            backgroundColor: theme.colorScheme.primary,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                product.title,
+              ),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: product.id,
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment(0, 0.8),
+                          end: Alignment(0, 0),
+                          colors: [
+                            Color.fromRGBO(0, 0, 0, 0.5),
+                            Color.fromRGBO(0, 0, 0, 0)
+                          ]),
+                    ),
+                  )
+                ],
               ),
             ),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
             const SizedBox(
               height: 20,
             ),
@@ -48,9 +66,12 @@ class ProductDetailsPage extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 20),
               width: double.infinity,
               child: Text(product.description),
+            ),
+            const SizedBox(
+              height: 1000,
             )
-          ],
-        ),
+          ]))
+        ],
       ),
     );
   }
